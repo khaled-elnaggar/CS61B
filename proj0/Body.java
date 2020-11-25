d8f0231b8f2c45419c01564318c4f1aab05a5bea
Khalid <dev_khalid@hotmail.com>
2020-11-25 10:08:56 +0200

    Complete the implementation of the Body class

--- proj0/Body.java
+++ proj0/Body.java
@@ -24,4 +24,66 @@ public class Body {
         mass = b.mass;
         imgFileName = b.imgFileName;
     }
+
+    public double calcDistance(Body b) {
+        double dx = xxPos - b.xxPos;
+        double dy = yyPos - b.yyPos;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+
+    public double calcForceExertedBy(Body b) {
+        if (this.equals(b)) {
+            return 0;
+        }
+        double rSquare = Math.pow(this.calcDistance(b), 2);
+        final double G = 6.67e-11;
+        return G * mass * b.mass / rSquare;
+    }
+
+    public double calcForceExertedByX(Body b) {
+        double dx = b.xxPos - xxPos;
+        return calcForceExertedBy(b) * dx / calcDistance(b);
+    }
+
+    public double calcForceExertedByY(Body b) {
+        double dy = b.yyPos - yyPos;
+        return calcForceExertedBy(b) * dy / calcDistance(b);
+    }
+
+
+    public double calcNetForceExertedByX(Body[] bodies) {
+        double sumX = 0;
+        for (Body b: bodies) {
+            if (this.equals(b)) {
+                continue;
+            }
+            sumX += calcForceExertedByX(b);
+        }
+        return sumX;
+    }
+
+    public double calcNetForceExertedByY(Body[] bodies) {
+        double sumY = 0;
+        for (Body b: bodies) {
+            if (this.equals(b)) {
+                continue;
+            }
+            sumY += calcForceExertedByY(b);
+        }
+        return sumY;
+    }
+
+    public void update(double dt, double fX, double fY) {
+        double aX = fX / mass;
+        double aY = fY / mass;
+        xxVel = xxVel + dt * aX;
+        yyVel = yyVel + dt * aY;
+        xxPos = xxPos + xxVel * dt;
+        yyPos = yyPos + yyVel * dt;
+    }
+
+
+
+
+
 }
