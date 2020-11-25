84a0b813a847fd60a1cee2a22dcaa44ef92bd662
Khalid <dev_khalid@hotmail.com>
2020-11-25 08:44:06 +0200

    Implement simple Body class

---
+++ proj0/Body.java
@@ -0,0 +1,27 @@
+public class Body {
+
+    public double xxPos;
+    public double yyPos;
+    public double xxVel;
+    public double yyVel;
+    public double mass;
+    public String imgFileName;
+
+    public Body(double xxPos, double yyPos, double xxVel, double yyVel, double mass, String img) {
+        this.xxPos = xxPos;
+        this.yyPos = yyPos;
+        this.xxVel = xxVel;
+        this.yyVel = yyVel;
+        this.mass = mass;
+        imgFileName = img;
+    }
+
+    public Body(Body b) {
+        xxPos = b.xxPos;
+        yyPos = b.yyPos;
+        xxVel = b.xxVel;
+        yyVel = b.yyVel;
+        mass = b.mass;
+        imgFileName = b.imgFileName;
+    }
+}
