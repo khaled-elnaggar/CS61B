ca6411807b19f7a8456e428abcde0f7b02680bbd
Khalid <dev_khalid@hotmail.com>
2020-11-29 11:58:13 +0200

    Finish NBody.java implementation

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -33,4 +33,79 @@ public class NBody {
         }
         return bodies;
     }
+
+    public static void main(String[] args) {
+        double T = Double.parseDouble(args[0]);
+        double dt = Double.parseDouble(args[1]);
+        String fileName = args[2];
+        double radius = NBody.readRadius(fileName);
+        Body[] bodies = NBody.readBodies(fileName);
+        int numberOfPlanets = bodies.length;
+
+        String bkGroundImg = "./images/starfield.jpg";
+
+        StdDraw.enableDoubleBuffering();
+        StdDraw.setScale(-radius, radius);
+        StdDraw.clear();
+
+        StdDraw.picture(0, 75, bkGroundImg);
+        for (Body b: bodies) {
+            StdDraw.picture(b.xxPos, b.yyPos, "./images/" + b.imgFileName);
+        }
+        StdDraw.show();
+
+        for (double timePassed = 0.0; timePassed <= T; timePassed += dt) {
+            double[] xForces = new double[numberOfPlanets];
+            double[] yForces = new double[numberOfPlanets];
+            
+            //calculate forces on each planet
+            for (int i = 0; i < numberOfPlanets; i++) {
+                xForces[i] = bodies[i].calcNetForceExertedByX(bodies);
+                yForces[i] = bodies[i].calcNetForceExertedByY(bodies);
+            }
+            //update every planet's position
+            for (int i = 0; i < numberOfPlanets; i++) {
+                bodies[i].update(dt, xForces[i], yForces[i]);
+            }
+
+            //draw every planet
+            StdDraw.picture(0, 75, bkGroundImg);
+            for (Body b: bodies) {
+                StdDraw.picture(b.xxPos, b.yyPos, "./images/" + b.imgFileName);
+            }
+            StdDraw.show();
+            StdDraw.pause(10);
+        }
+        
+StdOut.printf("%d\n", bodies.length);
+StdOut.printf("%.2e\n", radius);
+for (int i = 0; i < bodies.length; i++) {
+    StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
+                  bodies[i].xxPos, bodies[i].yyPos, bodies[i].xxVel,
+                  bodies[i].yyVel, bodies[i].mass, bodies[i].imgFileName);   
 }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
