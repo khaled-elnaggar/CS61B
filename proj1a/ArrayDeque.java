4d1c1b415d3715ff6c8d09388cdcc5960bab26c8
Khalid <dev_khalid@hotmail.com>
2020-12-14 18:16:22 +0200

    Initial implementation of LinkedListDeque in proj1A

---
+++ proj1a/ArrayDeque.java
@@ -0,0 +1,33 @@
+public class ArrayDeque<T>{
+    public void addFirst(T item){
+
+    }
+
+    public void addLast(T item){
+
+    }
+
+    public boolean isEmpty(){
+        return false;
+    }
+
+    public int size(){
+        return -1;
+    }
+
+    public void printDeque(){
+
+    }
+
+    public T removeFirst(){
+        return null;
+    }
+
+    public T removeLast(){
+        return null;
+    }
+
+    public T get(int index){
+        return null;
+    }
+}
---
+++ proj1a/LinkedListDeque.java
@@ -0,0 +1,85 @@
+public class LinkedListDeque<T> {
+
+    private static class Node<T>{
+        T item;
+        Node<T> next;
+        Node<T> prev;
+        public Node(T item, Node<T> prev, Node<T> next){
+            this.item = item;
+            this.prev = prev;
+            this.next = next;
+        }
+    }
+
+    private Node<T> sentinel;
+    private int size;
+
+    public LinkedListDeque(){
+        sentinel = new Node<T>(null, null, null);
+        sentinel.next = sentinel;
+        sentinel.prev = sentinel;
+        size = 0;
+    }
+
+
+    public void addFirst(T item){
+        Node<T> newFront = new Node<>(item, sentinel, sentinel.next);
+        sentinel.next.prev = newFront;
+        sentinel.next = newFront;
+        size++;
+    }
+
+    public void addLast(T item){
+        Node<T> newBack = new Node<>(item, sentinel.prev, sentinel);
+        sentinel.prev.next = newBack;
+        sentinel.prev = newBack;
+        size++;
+
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public void printDeque(){
+        Node<T> p = sentinel.next;
+        while(p != sentinel){
+            System.out.print(p.item + " ");
+            p = p.next;
+        }
+        System.out.println();
+
+    }
+
+    public T removeFirst(){
+        Node<T> ret = sentinel.next;
+        sentinel.next = ret.next;
+        ret.next.prev = sentinel;
+        size--;
+        return ret.item;
+    }
+
+    public T removeLast(){
+        Node<T> ret = sentinel.prev;
+        sentinel.prev = ret.prev;
+        ret.next = sentinel;
+        size--;
+        return ret.item;
+    }
+
+    public T get(int index){
+        if(index >= size){
+            return null;
+        }
+
+        Node<T> p = sentinel.next;
+        for(int i = 0; i < index && p != sentinel; i++){
+            p = p.next;
+        }
+        return p.item;
+    }
+}
