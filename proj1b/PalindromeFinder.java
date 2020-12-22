3ba28f05911742d6e09fd7ba3adcf70cc87393b4
Khalid <dev_khalid@hotmail.com>
2020-12-22 22:54:41 +0200

    Just for fun, try many Ns

--- proj1b/PalindromeFinder.java
+++ proj1b/PalindromeFinder.java
@@ -1,21 +1,30 @@
 public class PalindromeFinder {
     static Palindrome palindrome = new Palindrome();
-    static In in = new In("../library-fa20/data/words.txt");
 
     public static void palindromesGreaterThanN(int minLength, CharacterComparator cc){
+        In in = new In("../library-fa20/data/words.txt");
+
         while (!in.isEmpty()) {
             String word = in.readString();
             if (word.length() >= minLength && palindrome.isPalindrome(word, cc)) {
-                System.out.println(word);
+                System.out.print(word + " ");
             }
         }
+        System.out.println();
     }
 
 
 
     public static void main(String[] args) {
 
-        palindromesGreaterThanN(4, new OffByOne());
+        int minLength = 4;
+
+        palindromesGreaterThanN(minLength, null);
+        palindromesGreaterThanN(minLength, new OffByOne());
+        palindromesGreaterThanN(minLength, new OffByN(2));
+        palindromesGreaterThanN(minLength, new OffByN(3));
+        palindromesGreaterThanN(minLength, new OffByN(4));
+        palindromesGreaterThanN(minLength, new OffByN(5));
 
 
     }
