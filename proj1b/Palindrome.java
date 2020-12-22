8b9e5f587aaa755da1790bdc2710475dff15651d
Khalid <dev_khalid@hotmail.com>
2020-12-22 21:49:42 +0200

    Implement wordToDeque and pass tests

---
+++ proj1b/Palindrome.java
@@ -0,0 +1,11 @@
+public class Palindrome {
+
+    public Deque<Character> wordToDeque(String word){
+        ArrayDeque<Character> result = new ArrayDeque<>();
+        for(int i = 0; i < word.length(); i++){
+            result.addLast(word.charAt(i));
+        }
+        return result;
+    }
+
+}
--- proj1b/TestPalindrome.java
+++ proj1b/TestPalindrome.java
@@ -1,4 +1,4 @@
-/*import org.junit.Test;
+import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TestPalindrome {
@@ -10,9 +10,11 @@ public class TestPalindrome {
     public void testWordToDeque() {
         Deque d = palindrome.wordToDeque("persiflage");
         String actual = "";
+
         for (int i = 0; i < "persiflage".length(); i++) {
             actual += d.removeFirst();
         }
+
         assertEquals("persiflage", actual);
     }
-}     Uncomment this class once you've created your Palindrome class. */
+}
