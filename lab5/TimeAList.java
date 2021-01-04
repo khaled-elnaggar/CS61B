1dafa0ed2e98eb89c6708eecbe0fda33962e8f8f
Khalid <dev_khalid@hotmail.com>
2021-01-04 13:48:33 +0200

    Change resize implementation to compare results

--- lab5/TimeAList.java
+++ lab5/TimeAList.java
@@ -23,7 +23,7 @@ public class TimeAList {
     }
 
     public static void timeAListConstruction() {
-        List<Integer> Ns = new ArrayList<>(Arrays.asList(1000, 2000, 4000, 8000, 16000, 32000, 64000, 128000));
+        List<Integer> Ns = new ArrayList<>(Arrays.asList(1000, 2000, 4000, 8000, 16000, 32000, 64000, 128000, 256000, 512000, 1024000, 2048000, 4096000, 8192000));
 //        List<Integer> opCounts = new ArrayList<>(Arrays.asList(1000, 2000, 4000, 8000, 16000, 32000, 64000));;
         List<Double> times = new ArrayList<>();
         for (int ns : Ns) {
