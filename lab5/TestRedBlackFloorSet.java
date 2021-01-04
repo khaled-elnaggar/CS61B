efe4c90c4d7de4db62cbbd520b1b116e194683c0
Khalid <dev_khalid@hotmail.com>
2021-01-04 14:36:59 +0200

    Write randomized tests for both sets

--- lab5/TestRedBlackFloorSet.java
+++ lab5/TestRedBlackFloorSet.java
@@ -7,6 +7,15 @@ import static org.junit.Assert.assertEquals;
 public class TestRedBlackFloorSet {
     @Test
     public void randomizedTest() {
-       // TODO: YOUR CODE HERE
+        StdRandom.uniform(-5000, 5000);
+        AListFloorSet als = new AListFloorSet();
+        RedBlackFloorSet rbs = new RedBlackFloorSet();
+        for(int i = 0; i < 1000000; i++){
+            als.add(StdRandom.uniform(-5000.0, 5001.0));
+            rbs.add(StdRandom.uniform(-5000.0, 5001.0));
+        }
+        for(int i = 0; i < 100000; i++){
+            assertEquals(als.floor(StdRandom.uniform(-5000, 5001)), rbs.floor(StdRandom.uniform(-5000, 5001)), 0.000001);
+        }
     }
 }
