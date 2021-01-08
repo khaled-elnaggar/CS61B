72f61033c5c4b2e0af0b8e289975cf7874408cfd
Khalid <dev_khalid@hotmail.com>
2021-01-08 11:24:39 +0200

    Write equlity method for ARB

--- hw1/es/datastructur/synthesizer/ArrayRingBuffer.java
+++ hw1/es/datastructur/synthesizer/ArrayRingBuffer.java
@@ -108,4 +108,23 @@ public class ArrayRingBuffer<T> implements BoundedQueue<T> {
     public int fillCount() {
         return fillCount;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if(o == null || o.getClass() != ArrayRingBuffer.class){
+            return false;
+        }
+        ArrayRingBuffer<T> ARB = (ArrayRingBuffer<T>) o;
+        if(this.fillCount() != ARB.fillCount()){
+            return false;
+        }
+        Iterator<T> thisIterator = iterator();
+        Iterator<T> otherIterator = ARB.iterator();
+        while(thisIterator.hasNext() && otherIterator.hasNext()){
+            if(thisIterator.next() != otherIterator.next()){
+                return false;
+            }
+        }
+        return true;
+    }
 }
