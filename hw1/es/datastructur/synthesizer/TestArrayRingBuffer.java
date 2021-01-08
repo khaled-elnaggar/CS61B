9353144c342850d13465f532b8a66936f08696f9
Khalid <dev_khalid@hotmail.com>
2021-01-08 09:58:22 +0200

    Write a bunch of ARingBuffer Tests

--- hw1/es/datastructur/synthesizer/TestArrayRingBuffer.java
+++ hw1/es/datastructur/synthesizer/TestArrayRingBuffer.java
@@ -9,6 +9,36 @@ import static org.junit.Assert.*;
 public class TestArrayRingBuffer {
     @Test
     public void someTest() {
-        //ArrayRingBuffer arb = new ArrayRingBuffer(10);
+        ArrayRingBuffer<Double> arb = new ArrayRingBuffer<>(5);
+        assertEquals(0, arb.fillCount());
+        assertTrue(arb.isEmpty());
+        arb.enqueue(4.0);
+        double delta = 0.00000001;
+        assertEquals(4.0, arb.peek(), delta);
+        assertEquals(1, arb.fillCount());
+        assertFalse(arb.isEmpty());
+        arb.enqueue(-12.0);
+        assertEquals(-12.0, arb.peek(), delta);
+        assertEquals(2, arb.fillCount());
+        assertFalse(arb.isEmpty());
+
+        assertEquals(-12.0, arb.dequeue(), delta);
+        assertEquals(1, arb.fillCount());
+        assertFalse(arb.isEmpty());
+        assertEquals(4.0, arb.dequeue(), delta);
+        assertEquals(0, arb.fillCount());
+        assertTrue(arb.isEmpty());
+/*
+        for(int i = 0; i < 5; i++){
+            arb.enqueue(i*1.2);
+            assertEquals(i * 1.2, arb.peek(), delta);
+            assertEquals(i+1, arb.fillCount());
+        }
+        for(int i = 4; i >= 0; i--){
+            assertEquals(i*1.2, arb.dequeue(), delta);
+            assertEquals(i+1, arb.fillCount());
+        }
+*/
+
     }
 }
