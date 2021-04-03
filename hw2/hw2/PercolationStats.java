0ff8e91ac051bf81314c73f21f5441ac43f44ecc
Khalid <dev_khalid@hotmail.com>
2021-04-03 19:39:32 +0200

    Implement percoStats

--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,48 @@
 package hw2;
 
-public class PercolationStats {
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
 
+public class PercolationStats {
+    double[] xT;
+    Percolation p;
+    // perform T independent experiments on an N-by-N grid
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0) {
+            throw new IllegalArgumentException();
+        }
+        xT = new double[T];
+
+        for(int i = 0; i < T; i++){
+            p = pf.make(N);
+            while(true){
+                p.open(StdRandom.uniform(N), StdRandom.uniform(N));
+                if(p.percolates()){
+                    xT[i] = p.numberOfOpenSites() * 1.0 / (N * N);
+                    break;
+                }
+            }
+        }
+    }
+
+    // sample mean of percolation threshold
+    public double mean() {
+        return StdStats.mean(xT);
+    }
+
+    // sample standard deviation of percolation threshold
+    public double stddev() {
+        return StdStats.stddev(xT);
+    }
+
+    // low endpoint of 95% confidence interval
+    public double confidenceLow() {
+        return mean() - 1.96 * stddev() / Math.sqrt(xT.length);
+    }
+
+    // high endpoint of 95% confidence interval
+    public double confidenceHigh() {
+        return mean() + 1.96 * stddev() / Math.sqrt(xT.length);
+    }
 }
+
