ba06d5d8fc3d8d2f59359fcfa254b586b8c186f7
Khalid <dev_khalid@hotmail.com>
2021-04-05 19:10:47 +0200

    Implement constructor, private class node and put() operations

--- lab7/BSTMap.java
+++ lab7/BSTMap.java
@@ -2,9 +2,32 @@ import java.util.Iterator;
 import java.util.Set;
 
 public class BSTMap<K extends Comparable<K>, V> implements Map61B<K,V> {
+    private Node sentinel;
+    private int size;
+
+    private class Node<K, V>{
+        private K key;
+        private V value;
+        private Node left;
+        private Node right;
+
+        public Node(K key, V value){
+            this.key = key;
+            this.value = value;
+            left = null;
+            right = null;
+        }
+    }
+
+    public BSTMap(){
+        sentinel = new Node(null, null);
+        size = 0;
+    }
+
     @Override
     public void clear() {
-
+        sentinel.right = null;
+        size = 0;
     }
 
     @Override
@@ -19,12 +42,32 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K,V> {
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public void put(K key, V value) {
+        if(size == 0){
+            sentinel.right = new Node(key, value);
+            size = 1;
+            return;
+        }
+        put(key, value, sentinel.right);
+    }
 
+    private Node put(K key, V value, Node node){
+        if(node == null){
+            size += 1;
+            return new Node(key, value);
+        }
+
+        int cmp = key.compareTo((K) node.key);
+        if(cmp < 0){
+            node.left = put(key, value, node.left);
+        } else if (cmp > 0){
+            node.right = put(key, value, node.right);
+        }
+        return node;
     }
 
     public void printInOrder(){
