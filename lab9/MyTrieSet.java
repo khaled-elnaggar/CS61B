10fb31473b797c7647534f844b63acfb88f7327a
Khalid <dev_khalid@hotmail.com>
2022-04-12 17:14:41 +0200

    Finish lab9 implement trie with longestPrefixOf(), keysWithPrefix(), keysThatMatch(), keys() and size()

---
+++ lab9/MyTrieSet.java
@@ -0,0 +1,167 @@
+import java.util.*;
+
+public class MyTrieSet implements TrieSet61B {
+
+    Node root = new Node();
+    int size = 0;
+
+    public int size() {
+        return size;
+    }
+
+    private static class Node {
+        private boolean isKey = false;
+
+        private final Map<Character, Node> map = new HashMap<>();
+
+        public Node get(char key) {
+            return map.get(key);
+        }
+
+        public void put(char ch1, Node newNode) {
+            map.put(ch1, newNode);
+        }
+
+        public boolean isKey() {
+            return isKey;
+        }
+
+        public void setKey(boolean key) {
+            isKey = key;
+        }
+
+        public Set<Map.Entry<Character, Node>> getEntries() {
+            return map.entrySet();
+        }
+    }
+
+    @Override
+    public void add(String key) {
+        if (contains(key)) return;
+        Node node = root;
+        for (int i = 0; i < key.length(); i++) {
+            char ch1 = key.charAt(i);
+            Node subtrie = node.get(ch1);
+
+            if (subtrie == null) {
+                subtrie = new Node();
+                node.put(ch1, subtrie);
+            }
+
+            if (i + 1 == key.length()) {
+                subtrie.setKey(true);
+            }
+            node = subtrie;
+        }
+        size++;
+    }
+
+    @Override
+    public void clear() {
+        root = new Node();
+    }
+
+    @Override
+    public boolean contains(String key) {
+        Node node = root;
+        for (int i = 0; i < key.length(); i++) {
+            char ch1 = key.charAt(i);
+            Node subtrie = node.get(ch1);
+
+            if (subtrie == null) return false;
+            if (subtrie.isKey() && i + 1 == key.length()) return true;
+
+            node = subtrie;
+        }
+        return false;
+    }
+
+    @Override
+    public List<String> keysWithPrefix(String prefix) {
+        List<String> matchingKeys = new ArrayList<>();
+        Node node = root;
+        node = getNodeUntiLastPrefix(prefix, node);
+        if (node == null) return new ArrayList<>();
+        getAllChildrenKeys(node, matchingKeys, prefix);
+        return matchingKeys;
+    }
+
+    private Node getNodeUntiLastPrefix(String prefix, Node node) {
+        for (int i = 0; i < prefix.length(); i++) {
+            char ch1 = prefix.charAt(i);
+            Node subtrie = node.get(ch1);
+            if (subtrie == null) return null;
+            node = subtrie;
+        }
+        return node;
+    }
+
+    private void getAllChildrenKeys(Node node, List<String> matchingKeys, String prefix) {
+        if (node == null) return;
+
+        if (node.isKey) {
+            matchingKeys.add(prefix);
+        }
+
+        node.getEntries().forEach(characterNodeEntry -> {
+            getAllChildrenKeys(characterNodeEntry.getValue(), matchingKeys, prefix + characterNodeEntry.getKey());
+        });
+    }
+
+    public List<String> keys() {
+        return keysWithPrefix("");
+    }
+
+    @Override
+    public String longestPrefixOf(String key) {
+        Node node = root;
+        for (int i = 0; i < key.length(); i++) {
+            char ch1 = key.charAt(i);
+            Node subtrie = node.get(ch1);
+            if (subtrie == null) return key.substring(0, i);
+            node = subtrie;
+        }
+        return "";
+    }
+
+    public List<String> keysThatMatch(String pattern) {
+        List<String> matchingKeys = new ArrayList<>();
+        keysThatMatch(pattern, matchingKeys, "", root);
+        return matchingKeys;
+    }
+
+    private void keysThatMatch(String s, List<String> matchingKeys, String key, Node node) {
+        if (s.length() == 0) {
+            if (node.isKey())
+                matchingKeys.add(key);
+            return;
+        }
+
+        char c = s.charAt(0);
+        Set<Node> matchingSets = new HashSet<>();
+        if (c == '.') {
+            node.getEntries().forEach(characterNodeEntry -> {
+                keysThatMatch(s.substring(1), matchingKeys, key + characterNodeEntry.getKey(), characterNodeEntry.getValue());
+            });
+        } else {
+            final Node node1 = node.get(c);
+            if(node1 == null)
+                return;
+            keysThatMatch(s.substring(1), matchingKeys, key + c, node1);
+        }
+    }
+
+    public static void main(String[] args) {
+        String[] saStrings = new String[]{"same", "sam", "sad", "sap"};
+        String[] otherStrings = new String[]{"a", "awls", "hello"};
+
+        MyTrieSet t = new MyTrieSet();
+        for (String s : saStrings) {
+            t.add(s);
+        }
+        for (String s : otherStrings) {
+            t.add(s);
+        }
+        System.out.println(t.keys());
+    }
+}
---
+++ lab9/TestMyTrieSet.java
@@ -0,0 +1,107 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by Jenny Huang on 3/12/19.
+ * Extended by Khalid ElNaggar on 4/12/2022
+ * SOOOOOOOOOO FUCKING HAPPY
+ */
+public class TestMyTrieSet {
+
+    // assumes add/contains work
+    @Test
+    public void sanityClearTest() {
+        MyTrieSet t = new MyTrieSet();
+        for (int i = 0; i < 455; i++) {
+            t.add("hi" + i);
+            //make sure put is working via contains
+            assertTrue(t.contains("hi" + i));
+        }
+        t.clear();
+        for (int i = 0; i < 455; i++) {
+            assertFalse(t.contains("hi" + i));
+        }
+    }
+
+    // assumes add works
+    @Test
+    public void sanityContainsTest() {
+        MyTrieSet t = new MyTrieSet();
+        assertFalse(t.contains("waterYouDoingHere"));
+        t.add("waterYouDoingHere");
+        assertTrue(t.contains("waterYouDoingHere"));
+    }
+
+    // assumes add works
+    @Test
+    public void sanityPrefixTest() {
+        String[] saStrings = new String[]{"same", "sam", "sad", "sap"};
+        String[] otherStrings = new String[]{"a", "awls", "hello"};
+
+        MyTrieSet t = new MyTrieSet();
+        for (String s : saStrings) {
+            t.add(s);
+        }
+        assertEquals(4, t.size());
+
+        for (String s : otherStrings) {
+            t.add(s);
+        }
+        assertEquals(7, t.size());
+
+        List<String> keys = t.keysWithPrefix("sa");
+        assertNotNull(keys);
+        for (String s : saStrings) {
+            assertTrue(keys.contains(s));
+        }
+        for (String s : otherStrings) {
+            assertFalse(keys.contains(s));
+        }
+    }
+
+    @Test
+    public void sanityLongestPrefixTest() {
+        String[] saStrings =
+                new String[]{"same", "sam", "sad", "sap",
+                        "kito", "kiki", "kikiana"};
+
+        MyTrieSet t = new MyTrieSet();
+        for (String s : saStrings) {
+            t.add(s);
+        }
+        assertEquals("same", t.longestPrefixOf("same7aaaaa"));
+        assertEquals("kiki", t.longestPrefixOf("kiki3abdo"));
+    }
+
+    @Test
+    public void keysThatMatch() {
+        String[] saStrings =
+                new String[]{"same", "sam", "sad", "sap",
+                        "she", "see",
+                        "ninx", "canx", "plnx"};
+
+        MyTrieSet t = new MyTrieSet();
+        for (String s : saStrings) {
+            t.add(s);
+        }
+
+        List<String> matchingKeys = t.keysThatMatch("s.e");
+        final List<String> expectedMatch = List.of("she", "see");
+        assertEquals(expectedMatch.size(), matchingKeys.size());
+        assertTrue(expectedMatch.containsAll(matchingKeys));
+
+        List<String> matchingKeys2 = t.keysThatMatch("s..");
+        final List<String> expectedMatch2 = List.of("she", "see", "sam", "sap", "sad");
+        assertEquals(expectedMatch2.size(), matchingKeys2.size());
+        assertTrue(expectedMatch2.containsAll(matchingKeys2));
+
+        List<String> matchingKeys3 = t.keysThatMatch("..nx");
+        final List<String> expectedMatch3 = List.of("ninx", "canx", "plnx");
+        assertEquals(expectedMatch3.size(), matchingKeys3.size());
+        assertTrue(expectedMatch3.containsAll(matchingKeys3));
+
+    }
+}
